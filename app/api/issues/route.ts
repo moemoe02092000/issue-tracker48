65ba293055e7dceca6319ac701fd6de686b33ff2
Mohamed Ouazir <ouazir.mohamedredha@gmail.com>
2024-07-01 09:00:08 -0500

    Handle errors

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -24,14 +24,14 @@ import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
 const creatIssueSchema = z.object({
-    title:z.string().min(1).max(255),
-    description:z.string().min(1)
+    title:z.string().min(1,'title is required').max(255),
+    description:z.string().min(1,'description is required')
 })
 export async function POST(request:NextRequest){
     const body=await request.json();
     const validation=creatIssueSchema.safeParse(body)
     if (!validation.success)
-        return NextResponse.json(validation.error.errors,{status:400})
+        return NextResponse.json(validation.error.format(),{status:400})
     const newIssue= await prisma.issue.create({
         data:{title:body.title,description:body.description}
     })
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,19 +1,60 @@
-'use client'
-import { TextArea, TextField } from '@radix-ui/themes'
+"use client";
+import { Callout, TextArea, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
 import "easymde/dist/easymde.min.css";
-import React from 'react'
-import { Button } from '@radix-ui/themes'
+import React, { useState } from "react";
+import { Button } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
 
-const NewIssuePage = () => {
-  return (
-    <div className='max-w-xl space-y-3'> {/*enlarging the placeholder*/}
-        <TextField.Root placeholder='Title'/>
-        <SimpleMDE placeholder='Description'/>
-        <Button>Submit New Issue</Button>
-
-    </div>
-  )
+interface IssueForm {
+  title: string;
+  description: string;
 }
 
-export default NewIssuePage
+const NewIssuePage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const[error,setError]=useState('');
+  //console.log(register('title'))
+  return (
+
+    <div className="max-w-xl" >
+      {error && <Callout.Root color="red" className="mb-5">
+
+        <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(
+          async (data) => {try {
+            await axios.post("/api/issues", data);
+            router.push('/issues');
+          } catch (error) {
+            setError('An unexpected error occurred.');
+      
+          } }
+      
+        )}
+      >
+        {" "}
+        {/*enlarging the placeholder*/}
+        <TextField.Root placeholder="Title" {...register("title")} />
+        {/*<TextArea placeholder='Description'/>*/}
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+        {/*<SimpleMDE placeholder='Description'/>*/}
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
+  );
+};
+
+export default NewIssuePage;
+//1.07hour
