1d56d357e0007f5789a76c8af4fddbc9d2489d42
Mohamed Ouazir <ouazir.mohamedredha@gmail.com>
2024-07-01 10:11:23 -0500

    Add a Spinner

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Callout, Text, TextArea, TextField } from "@radix-ui/themes";
+import { Callout, Spinner, Text, TextArea, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
@@ -26,6 +26,7 @@ const NewIssuePage = () => {
   });
   const[error,setError]=useState('');
   //console.log(register('title'))
+  const[isSubmitting,setSubmitting]=useState(false);
   return (
 
     <div className="max-w-xl" >
@@ -37,9 +38,11 @@ const NewIssuePage = () => {
         className="space-y-3"
         onSubmit={handleSubmit(
           async (data) => {try {
+            setSubmitting(true);
             await axios.post("/api/issues", data);
             router.push('/issues');
           } catch (error) {
+            setSubmitting(false);
             setError('An unexpected error occurred.');
       
           } }
@@ -60,7 +63,7 @@ const NewIssuePage = () => {
         />
         {/*errors.description &&*/ /*<Text color="red" as="p">*/}<ErrorMessage>{errors.description?.message}</ErrorMessage>{/*</Text>*/}
         {/*<SimpleMDE placeholder='Description'/>*/}
-        <Button>Submit New Issue</Button>
+        <Button disabled={isSubmitting}>Submit New Issue {isSubmitting &&<Spinner/>}</Button>
       </form>
     </div>
   );
